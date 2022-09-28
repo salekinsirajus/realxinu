2f628e0569d2b6b564d6e9aeb7ee470a61f3f0bd
Shawn Salekin <ssaleki@vclv98-102.hpc.ncsu.edu>
2022-09-27 21:41:30 -0400

    Implemented CT1: print_ready_list

---
+++ system/readylist.c
@@ -0,0 +1,28 @@
+/* Filename: readylist.c
+ * Author: Shawn Salekin
+ * Description:
+ * 	Prints all process ID's in the ready list
+ *
+ * */
+#include <xinu.h>
+
+syscall print_ready_list(){
+    struct procent *prptr;
+    int32 i;
+
+    fprintf("\nProcesses that are in the readylist\n");
+	for (i = 0; i < NPROC; i++) {
+		prptr = &proctab[i];
+		if (prptr->prstate == PR_FREE) {  /* skip unused slots	*/
+			continue;
+		}
+
+		if (prptr->prstate == PR_READY) {
+            fprintf("%d ", i);
+        }
+
+    }
+    fprintf("\n");
+
+    return OK;
+}
