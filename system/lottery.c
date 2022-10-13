1d28473b425f2e97f010b01f9f574087aa2339c2
Shawn Salekin <ssaleki@vclvm177-212.vcl.ncsu.edu>
2022-10-13 10:57:06 -0400

    modified set_tickets function

--- system/lottery.c
+++ system/lottery.c
@@ -4,14 +4,11 @@
 #include <stdlib.h>
 
 void set_tickets(pid32 pid, uint32 tickets){
-	srand(1);
 	struct procent *pr;
     if (!pr->user_process){
         return;
     }
-	kprintf("current tickets: %d\n", pr->prprio);
-	// do more stuff as needed to convert tickets into priorities
-	pr->prprio = tickets;
-	kprintf("updated tickets: %d\n", pr->prprio);
 
+    //setting tickets here as prprio
+	pr->prprio = tickets;
 }
