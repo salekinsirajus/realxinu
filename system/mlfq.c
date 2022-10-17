c1e147b8fdd15977aed765589ab22eb207e35ce7
Shawn Salekin <ssaleki@vclvm177-62.vcl.ncsu.edu>
2022-10-17 11:02:00 -0400

    Fixed an edge case where a process was not being placed to any of the queues

--- system/mlfq.c
+++ system/mlfq.c
@@ -8,20 +8,39 @@ pid32 enqueue_mlfq(pid32 pid){
 	/* First time added gets to the highest priority level */
 	if (prptr->runtime == 0){
 		return enqueue(pid, highpq);
-	} else {
+	} 
+
+	/* Check the priority level for this process. Also check the
+	   Allotment usage. If used up all the allotted time, it gets
+	   one level down. */
+
+	if (!(prptr->time_allotment > 0)){
+		//penalize 
+		prptr->pr_level++;
+	}
+
+	if (prptr->pr_level == 0){
         return enqueue(pid, highpq); //for now
-    }
+	}
+	if (prptr->pr_level >= 1){
+		return enqueue(pid, lowpq);
+	}
 }
 
 pid32 dequeue_mlfq(){
 	//FIXME: this is a STUB
 	/* pops the next in line PID from the right level */
-	return dequeue(highpq);
+	if (nonempty(highpq)){
+	    return dequeue(highpq);
+	}
+	else {
+		return dequeue(lowpq);
+	}
 }
 
 int nonempty_mlfq(){
 	/* FIXME: this is a stub
 	 * checks if any of the user level queues are empty
 	 */
-	return nonempty(highpq);
+	return (nonempty(highpq) || nonempty(lowpq));
 } 
