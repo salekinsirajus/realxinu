365f10c51aecff76650caac06752bfb415f2e1c9
Shawn Salekin <ssaleki@vclvm177-213.vcl.ncsu.edu>
2022-11-12 23:32:03 -0500

    removed comments and or debug statements

--- system/lock.c
+++ system/lock.c
@@ -28,6 +28,7 @@ pid32 dq(qid16 q){
 }
 
 void print_queue(qid16 q, char *name){
+	return; //disabled
 	sync_printf("[%d]", currpid);
     qid16 ptr = firstid(q);
 	struct procent *prptr;
@@ -64,15 +65,11 @@ syscall lock(lock_t *l){
 	while(test_and_set(&l->guard, 1));
 	if (l->flag == 0){
 		l->flag = 1;
-		sync_printf("[%d] !!! PID %d acquired the lock\n", currpid, currpid);
+
 		l->pid = currpid;
 		l->guard = 0;
 	} else {
-		sync_printf("[%d] !!! PID %d TRIED to acquire the lock but FAILED.\n", currpid, currpid);
-		sync_printf("[%d] flag: %d, guard: %d, lockholder: %d\n", currpid, l->flag, l->guard, l->pid);
-	    //print_queue(l->waiting, "waitlist (before enq)");	
 		enqueue(currpid, l->waiting);
-	    //print_queue(l->waiting, "waitlist  (after enq)");	
 		l->guard = 0;
 		park(l->waiting); 				/* put this to sleep */	
 	}
@@ -83,25 +80,17 @@ syscall lock(lock_t *l){
 
 syscall unlock(lock_t *l){
 	if (l->pid != currpid){
-		sync_printf("[%d] UNAUTHORIZED: pid %d is trying to release the lock held by %d\n", currpid, currpid, l->pid);
 		return SYSERR;
-	} else {
-		sync_printf("[%d] AUTHORIZED: pid %d is trying to release the lock held by %d\n", currpid, currpid, l->pid);
 	}
+
 	while(test_and_set(&l->guard, 1));
-	//sync_printf("PID %d is going to unlock lock\n");
 
 
 	if (isempty(l->waiting)){
-		sync_printf("[%d] PID %d unlocked, no one is looking for the lock.\n", currpid, currpid);
 		l->flag = 0;            /* no one is looking for the lock */
 	} else {
-	    //print_queue(l->waiting, "waitlist");	
 		pid32 x = dq(l->waiting);
-		sync_printf("[%d] lock is available, %d should get the lock.\n", currpid, x);
 
-	    print_queue(l->waiting, "waitlist");	
-	    print_queue(readylist, "readylist");	
 		unpark(x); /* hold the lock for next process */
 
 		/* FIXME TODO WARNING  THIS IS EXPERIMENTAL CODE */
@@ -141,23 +130,11 @@ syscall park(qid16 q){
 	intmask 	mask;    	/* Interrupt mask		*/
 	mask = disable();
 
-	//sync_printf("trying to put %d to sleep via park\n", currpid);
-	//if (set_park_flag == 1 ){
-	//	set_park_flag = 0;
-	
 	struct procent *prptr;
 	prptr = &proctab[currpid];
-	//sync_printf("[%d] putting PID %d to sleep\n", currpid, currpid);
-	//print_queue(readylist, "readylist before WAIT");
-	//print_queue(q, 		   "waitnlist before WAIT");
 	prptr->prstate = PR_WAIT;
-	//print_queue(readylist, "readylist after  WAIT");
-	//print_queue(q, 		   "waitnlist after  WAIT");
 	resched();
 
-	//print_queue(readylist, "readylist after rescd");
-	//print_queue(q, 		   "waitnlist after rescd");
-	//} 
 
 	restore(mask);
 	return OK;
@@ -167,11 +144,7 @@ syscall unpark(pid32 pid){
 	/* wakes up a particular process from sleep */
 	intmask 	mask;    	/* Interrupt mask		*/
 	mask = disable();
-	//sync_printf("[%d] PID %d is going to be added to the readylist`\n", currpid, pid);
-	//set_park_flag = 0;	
-	//if (set_park_flag == 0){
 	ready(pid);
-	//}
 
 	restore(mask);
 	return OK;
