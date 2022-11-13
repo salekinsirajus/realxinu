c443ed0d77331c3b53220c742ca8230fdd7f705b
Shawn Salekin <ssaleki@vclvm177-213.vcl.ncsu.edu>
2022-11-12 23:34:30 -0500

    Removed comments/debug stmts

--- system/active_lock.c
+++ system/active_lock.c
@@ -37,11 +37,6 @@ syscall al_initlock(al_lock_t *l){
 	l->lock_id = al_lock_count;
 	l->in_use = 1;
 
-	sync_printf("[%d]: original lock location: %X, lock_id:%d\n", currpid, &l, l->lock_id);
-
-	sync_printf("[%d]: after assignment lock location: %X, lock_id:%d\n", currpid, &l, l->lock_id);
-
-	sync_printf("[%d] pid %d is initializing lock %d, al_lock_count %d.\n", currpid, currpid, l->lock_id, al_lock_count);
 	l->pid = -1;
 	l->flag = 0;               /* available */
 	l->guard = 0;
@@ -51,7 +46,6 @@ syscall al_initlock(al_lock_t *l){
 	lentry = &locktab[l->lock_id];
 	lentry->ptr = l;
 
-	sync_printf("in the table, lock_id: %d, on the lock_object: %d\n", lentry->lock_id, lentry->ptr->lock_id);
 	al_lock_count++;
 	return OK;
 }
@@ -102,7 +96,6 @@ void detect_deadlock(al_lock_t *l, pid32 pid){
 		waiting_on = prptr->waiting_on_lock;  //lock_id
 		// if not, there is no chain
 		if (waiting_on == -1){
-			sync_printf("[%d] lockholder not waiting on any lock\n", lockholder);
 			// let go
 			break;
 		}
@@ -145,23 +138,15 @@ syscall al_trylock(al_lock_t *l){
 syscall al_lock(al_lock_t *l){
 	struct procent *prptr = &proctab[currpid];
 	prptr->waiting_on_lock = l->lock_id;
-	sync_printf("[%d] PID %d wants the lock %d, marked at PCB: %d\n", currpid, currpid, l->lock_id, prptr->waiting_on_lock);
 	detect_deadlock(l, currpid);
 
 	while(test_and_set(&l->guard, 1) == 1);
-	sync_printf("[%d] lock_id: %d, flag: %d, guard: %d, lockholder: %d\n", currpid, l->lock_id, l->flag, l->guard, l->pid);
 	if (l->flag == 0){
 		l->flag = 1;
 		prptr->waiting_on_lock = -1;  //Got the lock, does not want it anymore
-		sync_printf("[%d] PID %d GOT the lock %d, marked at PCB: %d\n", currpid, currpid, l->lock_id, prptr->waiting_on_lock);
-		sync_printf("[%d] !!! PID %d acquired the [lock %d]\n", currpid, currpid, l->lock_id);
 		l->pid = currpid;
-		sync_printf("[%d] vars after acquisition == lock_id: %d, flag: %d, guard: %d, lockholder: %d\n", currpid, l->lock_id, l->flag, l->guard, l->pid);
 		l->guard = 0;
-		sync_printf("[%d] vars after guard release == lock_id: %d, flag: %d, guard: %d, lockholder: %d\n", currpid, l->lock_id, l->flag, l->guard, l->pid);
 	} else {
-		sync_printf("[%d] !!! PID %d TRIED to acquire [lock %d] but FAILED.\n", currpid, currpid, l->lock_id);
-		sync_printf("[%d] lock_id: %d, flag: %d, guard: %d, lockholder: %d\n", currpid, l->lock_id, l->flag, l->guard, l->pid);
 		enqueue(currpid, l->waiting);
 		
 		l->guard = 0;
@@ -173,30 +158,17 @@ syscall al_lock(al_lock_t *l){
 
 
 syscall al_unlock(al_lock_t *l){
-//	l = &allocktab[l->lock_id];
 
 	if (l->pid != currpid){
-		sync_printf("[%d] UNAUTHORIZED: pid %d is trying to release the lock held by %d\n", currpid, currpid, l->pid);
 		return SYSERR;
-	} else {
-		sync_printf("[%d] AUTHORIZED: pid %d is trying to release the lock held by %d\n", currpid, currpid, l->pid);
 	}
+	
 	while(test_and_set(&l->guard, 1) == 1);
-	//sync_printf("PID %d is going to unlock lock\n");
-
 
 	if (isempty(l->waiting)){
-		sync_printf("[%d] PID %d unlocked, no one is looking for the lock.\n", currpid, currpid);
 		l->flag = 0;            /* no one is looking for the lock */
 	} else {
-	    //print_queue(l->waiting, "waitlist");	
 		pid32 x = dq(l->waiting);
-		//struct procent *prptr = &proctab[x];
-		//prptr->waiting_on_lock = -1; //unmark which lock he is waiting on
-		sync_printf("[%d] lock is available, %d should get the lock.\n", currpid, x);
-
-	    print_queue(l->waiting, "waitlist");	
-	    print_queue(readylist, "readylist");	
 		unpark(x); /* hold the lock for next process */
 
 		/* FIXME TODO WARNING  THIS IS EXPERIMENTAL CODE */
